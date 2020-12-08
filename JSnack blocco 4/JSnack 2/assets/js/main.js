bab7fbf0a8c1373a286613b08d400fda911e7bc1
Tommaso Figlia <tommasofiglia@gmail.com>
2020-12-08 11:48:26 +0100

    jsnack 3 del blocco 4 caricato

--- JSnack blocco 4/JSnack 2/assets/js/main.js
+++ JSnack blocco 4/JSnack 2/assets/js/main.js
@@ -1,3 +1,40 @@
+/***************** CONSEGNA ******************
+Creare un array di oggetti: ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore.
+*********************************************/
+
 $(function () {
+
+  var biciclette = [
+  {
+    nome: "Trek",
+    peso:10
+  },
+  {
+    nome: "Bianchi",
+    peso: 7
+  },
+  {
+    nome: "Cannondale",
+    peso: 8
+  },
+  {
+    nome: "Cube",
+    peso: 9
+  }
+
+  ];
+
+  console.log(biciclette);
+
+  for (var i = 0; i < biciclette.length; i++) {
+      if (biciclette[i].peso == biciclette[0].peso) {
+        console.log(biciclette[i].peso);
+      } else if (biciclette[i].peso < biciclette[i-1].peso ){
+        console.log(biciclette[i].peso);
+      } else {
+        console.log(biciclette[i - 1].peso);
+      }
+  }
 
 });
